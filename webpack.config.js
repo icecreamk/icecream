cc986eb2228d101bc51939ee2b3e2f61b413f1fe
kkxxll <550893685@qq.com>
2020-10-12 21:48:26 +0800

    webpack基本配置

--- webpack.config.js
+++ webpack.config.js
@@ -1,3 +1,17 @@
+const path = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+
 module.exports = {
-    
+  devServer: {
+    port: 8080,
+    hot: false
+  },
+  plugins: [
+    new HtmlWebpackPlugin()
+  ],
+  entry: './src/index.js',
+  output: {
+    filename: 'main.js',
+    path: path.resolve(__dirname, 'dist')
+  }
 }
