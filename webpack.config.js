5eeba0a61664a6e6943da069caf43a1d7e4262c2
kkxxll <550893685@qq.com>
2020-10-18 10:37:26 +0800

    devServer

--- webpack.config.js
+++ webpack.config.js
@@ -28,7 +28,15 @@ module.exports = {
     })
   ],
   devServer: {
+    // color（CLI only） console中打印彩色日志 在命令行中使用 --color --progress
+    // historyApiFallback 任意的404响应都被替代为index.html
+    // host 指定一个host,默认是localhost。如果你希望服务器外部可以访问，指定如下：host: "0.0.0.0"。比如你用手机通过IP访问。
+    // hot 启用Webpack的模块热替换特性。
+    // proxy 代理。比如在 localhost:3000 上有后端服务的话，你可以这样启用代理：
     port: 8080,
+    // URL的根目录。不设定则默认指向项目根目录。
+    contentBase: path.join(__dirname, './dist'),
+    historyApiFallback: true,
     hot: false
   },
 }
